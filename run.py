5665a92e79c0fb3a3d4ab14b9634378934a5f3e3
Steven Oyaro <106848189+StevenOyar@users.noreply.github.com>
2025-10-15 05:59:48 +0300

    Update run.py

--- run.py
+++ run.py
@@ -31,10 +31,12 @@ app.config["MAIL_PASSWORD"] = os.getenv("MAIL_PASSWORD")
 app.config["MAIL_DEFAULT_SENDER"] = os.getenv("MAIL_SENDER_EMAIL")
 
 # MySQL Configuration
+# MySQL Configuration
 app.config['MYSQL_HOST'] = os.getenv('MYSQL_HOST', 'localhost')
 app.config['MYSQL_USER'] = os.getenv('MYSQL_USER', 'root')
 app.config['MYSQL_PASSWORD'] = os.getenv('MYSQL_PASSWORD', '')
 app.config['MYSQL_DB'] = os.getenv('MYSQL_DB', 'regenardhi_db')
+app.config['MYSQL_PORT'] = int(os.getenv('MYSQL_PORT', 3306))  # ADD THIS LINE
 app.config['MYSQL_CURSORCLASS'] = 'DictCursor'
 
 # Initialize extensions
